a0544921e98b1fff5cac524c369e272e1093d255
Thomas Moore <thomas.moore@csiro.au>
2022-08-01 15:14:03 +1000

    Init

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='CARSv2 project repo',
+    author='Thomas Moore',
+    license='',
+)
